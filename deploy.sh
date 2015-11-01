c8637a563cd53920dc22baeaaa9edbf2bfbd8da0
wangshubin <wangshubin@trnux.com>
2015-11-01 20:51:51 +0800

    2nd commit deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###########################################
+# File Name: deploy.sh
+# Author: Troy
+# mail: troy@trnux.com
+# Created Time: Sun 01 Nov 2015 08:51:01 PM CST
+#########################################
+echo haha
